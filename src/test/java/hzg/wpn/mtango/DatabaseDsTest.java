af60757e52fb26cdf505ad2a52297dd690d24c2e
Ingvord <tla-ingvord@yandex.ru>
2014-05-23 15:29:39 +0200

    mTango.web: #47 progress

--- src/test/java/hzg/wpn/mtango/DatabaseDsTest.java
+++ src/test/java/hzg/wpn/mtango/DatabaseDsTest.java
@@ -2,6 +2,8 @@ package hzg.wpn.mtango;
 
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
+
 /**
  * @author Igor Khokhriakov <igor.khokhriakov@hzg.de>
  * @since 23.05.14
@@ -14,6 +16,6 @@ public class DatabaseDsTest {
 
         String result = instance.getDeviceAddress("sys/tg_test/1");
 
-        //TODO assert
+        assertEquals("tango://hzgharwi3:10000/sys/tg_test/1", result);
     }
 }
