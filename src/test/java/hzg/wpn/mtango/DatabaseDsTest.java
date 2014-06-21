0b1e9c568259f66fdb5f8d5f8b68b7cdbd873a2a
Ingvord <Ingvord@users.noreply.github.com>
2014-06-21 16:44:58 +0000

    mTango.web: progress #56

--- src/test/java/hzg/wpn/mtango/DatabaseDsTest.java
+++ src/test/java/hzg/wpn/mtango/DatabaseDsTest.java
@@ -10,13 +10,19 @@ import static junit.framework.Assert.assertEquals;
  * @since 23.05.14
  */
 public class DatabaseDsTest {
+    private static final String TANGO_HOST;
+
+    static {
+        TANGO_HOST = System.getenv("TANGO_HOST");
+    }
+
     @Test
     public void testGetDeviceAddress() throws Exception {
-        //TODO environment independent
-        DatabaseDs instance = new DatabaseDs("hzgharwi3:10000");
+        DatabaseDs instance = new DatabaseDs(TANGO_HOST);
 
         String result = instance.getDeviceAddress("sys/tg_test/1");
 
-        assertEquals("tango://hzgharwi3:10000/sys/tg_test/1", result);
+        //assume that TangoTest is running on the same host as DB which normally the case
+        assertEquals("tango://" + TANGO_HOST + "/sys/tg_test/1", result);
     }
 }
