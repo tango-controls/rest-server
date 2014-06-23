14ee3739102a04c8f73c0ce5cfc58ae0baffdaca
Ingvord <tla-ingvord@yandex.ru>
2014-06-23 17:21:06 +0200

    mTango.web: #56 progress

---
+++ src/main/java/org/tango/web/server/rest/DeviceInfoResponseWriter.java
@@ -0,0 +1,42 @@
+package org.tango.web.server.rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import fr.esrf.TangoApi.DeviceInfo;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+/**
+ * @author Igor Khokhriakov <igor.khokhriakov@hzg.de>
+ * @since 23.06.14
+ */
+@Provider
+public class DeviceInfoResponseWriter implements MessageBodyWriter<DeviceInfo> {
+    private final Gson gson = new GsonBuilder()
+            .serializeNulls()
+            .create();
+
+    @Override
+    public boolean isWriteable(Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType) {
+        return mediaType == MediaType.APPLICATION_JSON_TYPE && aClass.isAssignableFrom(DeviceInfo.class);
+    }
+
+    @Override
+    public long getSize(DeviceInfo deviceInfo, Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType) {
+        return -1;
+    }
+
+    @Override
+    public void writeTo(DeviceInfo deviceInfo, Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> stringObjectMultivaluedMap, OutputStream outputStream) throws IOException, WebApplicationException {
+        gson.toJson(deviceInfo, new OutputStreamWriter(outputStream));
+    }
+}
--- src/main/java/org/tango/web/server/rest/Rest2Tango.java
+++ src/main/java/org/tango/web/server/rest/Rest2Tango.java
@@ -1,25 +1,32 @@
-package org.tango.web.server;
+package org.tango.web.server.rest;
 
 /**
  * @author Ingvord
  * @since 21.06.14
  */
 
+import fr.esrf.TangoApi.AttributeInfoEx;
+import fr.esrf.TangoApi.CommandInfo;
 import fr.esrf.TangoApi.DeviceInfo;
 import hzg.wpn.tango.client.proxy.TangoProxy;
+import hzg.wpn.tango.client.proxy.TangoProxyException;
 import org.apache.commons.beanutils.ConvertUtils;
+import org.tango.web.server.DatabaseDs;
+import org.tango.web.server.DeviceMapper;
 
 import javax.servlet.ServletContext;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 @Path("/")
-public class TangoGateway {
-
+public class Rest2Tango {
     @GET
     @Path("devices")
     @Produces("application/json")
+    //TODO marshaller
     public Collection<String> getDevices(@Context ServletContext ctx) throws Exception {//TODO handle exception
         DatabaseDs db = (DatabaseDs) ctx.getAttribute(DatabaseDs.TANGO_DB);
         return db.getDeviceList();
@@ -27,6 +34,7 @@ public class TangoGateway {
 
     @GET
     @Path("device/{domain}/{name}/{instance}")
+    @Produces("application/json")
     //TODO marshaller
     public DeviceInfo getDeviceInfo(@PathParam("domain") String domain,
                                     @PathParam("name") String name,
@@ -39,36 +47,50 @@ public class TangoGateway {
 
     @GET
     @Path("device/{domain}/{name}/{instance}/attributes")
-    public String getDeviceAttributes(@PathParam("domain") String domain,
-                                      @PathParam("name") String name,
-                                      @PathParam("instance") String instance,
-                                      @Context ServletContext ctx) {
-        return "List of device attributes";
+    @Produces("application/json")
+    //TODO marshaller
+    public Collection<AttributeInfoEx> getDeviceAttributes(@PathParam("domain") String domain,
+                                                           @PathParam("name") String name,
+                                                           @PathParam("instance") String instance,
+                                                           @Context ServletContext ctx) throws Exception {
+        TangoProxy proxy = lookupTangoProxy(domain, name, instance, ctx);
+        Collection<AttributeInfoEx> result = new ArrayList<>();
+        Collections.addAll(result, proxy.toDeviceProxy().get_attribute_info_ex());
+        return result;
     }
 
     @GET
     @Path("device/{domain}/{name}/{instance}/commands")
-    public String getDeviceCommands(@PathParam("domain") String domain,
-                                    @PathParam("name") String name,
-                                    @PathParam("instance") String instance,
-                                    @Context ServletContext ctx) {
-        return "List of device commands";
+    @Produces("application/json")
+    //TODO marshaller
+    public Collection<CommandInfo> getDeviceCommands(@PathParam("domain") String domain,
+                                                     @PathParam("name") String name,
+                                                     @PathParam("instance") String instance,
+                                                     @Context ServletContext ctx) throws Exception {
+        TangoProxy proxy = lookupTangoProxy(domain, name, instance, ctx);
+        Collection<CommandInfo> result = new ArrayList<>();
+        Collections.addAll(result, proxy.toDeviceProxy().command_list_query());
+        return result;
+
     }
 
     @GET
     @Path("device/{domain}/{name}/{instance}/attribute/{attr}")
+    @Produces("application/json")
+    //TODO marshaller
     public Object getAttribute(@PathParam("domain") String domain,
                                @PathParam("name") String name,
                                @PathParam("instance") String instance,
                                @PathParam("attr") String attr,
                                @Context ServletContext ctx) throws Exception {
-        DeviceMapper mapper = (DeviceMapper) ctx.getAttribute(DeviceMapper.TANGO_MAPPER);
-        TangoProxy device = mapper.map(domain + "/" + name + "/" + instance);
-        return device.readAttributeValueTimeQuality(attr);
+        TangoProxy proxy = lookupTangoProxy(domain, name, instance, ctx);
+        return proxy.readAttributeValueTimeQuality(attr);
     }
 
     @PUT
     @Path("device/{domain}/{name}/{instance}/attribute/{attr}/argin={arg}")
+    @Produces("application/json")
+    //TODO marshaller
     public void putAttribute(@PathParam("domain") String domain,
                              @PathParam("name") String name,
                              @PathParam("instance") String instance,
@@ -76,15 +98,16 @@ public class TangoGateway {
                              @PathParam("arg") String arg,
                              @Context ServletContext ctx) throws Exception {
         //TODO exceptions
-        DeviceMapper mapper = (DeviceMapper) ctx.getAttribute(DeviceMapper.TANGO_MAPPER);
-        TangoProxy device = mapper.map(domain + "/" + name + "/" + instance);
-        Class<?> targetType = device.getAttributeInfo(attr).getClazz();
+        TangoProxy proxy = lookupTangoProxy(domain, name, instance, ctx);
+        Class<?> targetType = proxy.getAttributeInfo(attr).getClazz();
         Object converted = ConvertUtils.convert(arg, targetType);
-        device.writeAttribute(attr, converted);
+        proxy.writeAttribute(attr, converted);
     }
 
     @PUT
     @Path("device/{domain}/{name}/{instance}/command/{cmd}/argin={arg}")
+    @Produces("application/json")
+    //TODO marshaller
     public Object putCommand(@PathParam("domain") String domain,
                              @PathParam("name") String name,
                              @PathParam("instance") String instance,
@@ -92,10 +115,14 @@ public class TangoGateway {
                              @PathParam("arg") String arg,
                              @Context ServletContext ctx) throws Exception {
         //TODO exceptions
-        DeviceMapper mapper = (DeviceMapper) ctx.getAttribute(DeviceMapper.TANGO_MAPPER);
-        TangoProxy device = mapper.map(domain + "/" + name + "/" + instance);
-        Class<?> targetType = device.getCommandInfo(cmd).getArginType();
+        TangoProxy proxy = lookupTangoProxy(domain, name, instance, ctx);
+        Class<?> targetType = proxy.getCommandInfo(cmd).getArginType();
         Object converted = ConvertUtils.convert(arg, targetType);
-        return device.executeCommand(cmd, converted);
+        return proxy.executeCommand(cmd, converted);
+    }
+
+    private TangoProxy lookupTangoProxy(String domain, String name, String instance, ServletContext ctx) throws TangoProxyException {
+        DeviceMapper mapper = (DeviceMapper) ctx.getAttribute(DeviceMapper.TANGO_MAPPER);
+        return mapper.map(domain + "/" + name + "/" + instance);
     }
 }
