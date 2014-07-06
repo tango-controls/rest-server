e867ae73e7ea1968e6f85b318faa241553e5f3ee
Ingvord <tla-ingvord@yandex.ru>
2014-07-06 15:38:10 +0000

    mTango.web: progress #56

--- src/test/java/org/tango/web/server/rest/Rest2TangoTest.java
+++ src/test/java/org/tango/web/server/rest/Rest2TangoTest.java
@@ -1,5 +1,6 @@
 package org.tango.web.server.rest;
 
+import com.google.gson.Gson;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScheme;
 import org.apache.http.auth.AuthScope;
@@ -18,11 +19,17 @@ import org.jboss.resteasy.client.jaxrs.engines.ApacheHttpClient4Engine;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * @author Ingvord
  * @since 05.07.14
  */
 public class Rest2TangoTest {
+    private final Gson gson = new Gson();
+
     private ClientHttpEngine engine;
 
     @Before
@@ -59,44 +66,83 @@ public class Rest2TangoTest {
 
     @Test
     public void testGetDevice() throws Exception {
-
+        ResteasyClient cr = new ResteasyClientBuilder().httpEngine(engine).build();
+        String json = cr.target("http://localhost:8080/mtango/rest/device/sys/tg_test/1")
+                .request().get(String.class);
+        DeviceState result = gson.fromJson(json, DeviceState.class);
+        assertTrue(result.state.equals("RUNNING"));
+        assertTrue(result.status.equals("The device is in RUNNING state"));
     }
 
     @Test
     public void testGetDeviceInfo() throws Exception {
-
+        ResteasyClient cr = new ResteasyClientBuilder().httpEngine(engine).build();
+        String json = cr.target("http://localhost:8080/mtango/rest/device/sys/tg_test/1/info")
+                .request().get(String.class);
+        DeviceInfo result = gson.fromJson(json, DeviceInfo.class);
+        assertTrue(result.name.equals("sys/tg_test/1"));
     }
 
     @Test
     public void testGetDeviceAttributes() throws Exception {
-
+        ResteasyClient cr = new ResteasyClientBuilder().httpEngine(engine).build();
+        String json = cr.target("http://localhost:8080/mtango/rest/device/sys/tg_test/1/attributes")
+                .request().get(String.class);
+        Response result = gson.fromJson(json, Response.class);
+        assertTrue(((List<String>) result.argout).contains("long_scalar_w"));
     }
 
     @Test
     public void testGetDeviceCommands() throws Exception {
-
+        ResteasyClient cr = new ResteasyClientBuilder().httpEngine(engine).build();
+        String json = cr.target("http://localhost:8080/mtango/rest/device/sys/tg_test/1/commands")
+                .request().get(String.class);
+        Response result = gson.fromJson(json, Response.class);
+        assertTrue(((List<String>) result.argout).contains("DevString"));
     }
 
     @Test
     public void testPutAttribute() throws Exception {
-        ResteasyClient cr = new ResteasyClientBuilder().httpEngine(engine).build();// Request("http://www.mastertheboss.com/");
+        ResteasyClient cr = new ResteasyClientBuilder().httpEngine(engine).build();
         cr.target("http://localhost:8080/mtango/rest/device/sys/tg_test/1/long_scalar_w=1234567")
                 .request().put(null);
 //        System.out.println(result);
     }
 
     @Test
-    public void testGetCommandOrAttributeInfo() throws Exception {
-
+    public void testGetAttributeInfo() throws Exception {
+        ResteasyClient cr = new ResteasyClientBuilder().httpEngine(engine).build();
+        String json = cr.target("http://localhost:8080/mtango/rest/device/sys/tg_test/1/long_scalar_w/info")
+                .request().get(String.class);
+        AttributeInfo result = gson.fromJson(json, AttributeInfo.class);
+        assertTrue(result.name.equals("long_scalar_w"));
     }
 
     @Test
-    public void testGetCommandOrAttribute() throws Exception {
+    public void testGetCommandInfo() throws Exception {
+        ResteasyClient cr = new ResteasyClientBuilder().httpEngine(engine).build();
+        String json = cr.target("http://localhost:8080/mtango/rest/device/sys/tg_test/1/DevString/info")
+                .request().get(String.class);
+        CommandInfo result = gson.fromJson(json, CommandInfo.class);
+        assertTrue(result.cmd_name.equals("DevString"));
+    }
 
+    @Test
+    public void testGetAttribute() throws Exception {
+        ResteasyClient cr = new ResteasyClientBuilder().httpEngine(engine).build();
+        String json = cr.target("http://localhost:8080/mtango/rest/device/sys/tg_test/1/long_scalar_w")
+                .request().get(String.class);
+        Response result = gson.fromJson(json, Response.class);
+        //gson deserializes any number to double if otherwise is not specified
+        assertTrue((int) (double) result.argout == 123456);
     }
 
     @Test
     public void testGetCommand() throws Exception {
-
+        ResteasyClient cr = new ResteasyClientBuilder().httpEngine(engine).build();
+        String json = cr.target("http://localhost:8080/mtango/rest/device/sys/tg_test/1/DevString=Hello World!!!")
+                .request().get(String.class);
+        Response result = gson.fromJson(json, Response.class);
+        assertTrue(result.argout.equals("Hello World!!!"));
     }
 }
