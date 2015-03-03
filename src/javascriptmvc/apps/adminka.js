730f0f48292e05658fc393cc6c28ef5250cc16a3
Igor Khokhriakov <igor.khokhriakov@hzg.de>
2015-03-03 11:47:43 +0100

    add admin panel app

---
+++ src/javascriptmvc/apps/adminka.js
@@ -0,0 +1,24 @@
+include.css(
+    
+);
+include.resources(
+    
+);
+include.engines(
+    
+);
+include.plugins(
+    "controller","view","model"
+    );
+
+include(function(){ //runs after prior includes are loaded
+  include.models(
+        
+    );
+  include.controllers(
+        "adminka/main"
+    );
+  include.views(
+        
+    );
+});
