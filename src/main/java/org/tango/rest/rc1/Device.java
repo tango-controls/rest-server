75f7f37b9d4d3269e9873de228dcfb33e57bcd46
Igor Khokhriakov <igor.khokhriakov@hzg.de>
2015-11-27 20:53:01 +0100

    Tango REST API RC1 impl progress

--- src/main/java/org/tango/rest/rc1/Device.java
+++ src/main/java/org/tango/rest/rc1/Device.java
@@ -1,6 +1,9 @@
 package org.tango.rest.rc1;
 
-import org.tango.web.rest.DeviceInfo;
+
+import fr.esrf.TangoApi.DeviceInfo;
+
+import java.util.Collection;
 
 /**
  * @author Igor Khokhriakov <igor.khokhriakov@hzg.de>
@@ -8,11 +11,20 @@ import org.tango.web.rest.DeviceInfo;
  */
 public class Device {
     public final String name;
-    public final DeviceInfo info = null;
+    public final DeviceInfo info;
+    public final Collection<NamedEntity> attributes;
+    public final Collection<NamedEntity> commands;
+    public final Collection<NamedEntity> properties;
+    public final String state;
     public final String href;
 
-    public Device(String name, String href) {
+    public Device(String name, DeviceInfo info, Collection<NamedEntity> attributes, Collection<NamedEntity> commands, Collection<NamedEntity> properties, String href) {
         this.name = name;
+        this.info = info;
+        this.attributes = attributes;
+        this.commands = commands;
+        this.properties = properties;
         this.href = href;
+        this.state = href + "/state";
     }
 }
