80849b044246e794830968cab19d0a3dcfdc0389
Igor Khokhriakov <igor.khokhriakov@hzg.de>
2015-12-11 16:38:58 +0100

    Tango REST API RC1 impl progress

--- src/main/java/org/tango/web/server/Responses.java
+++ src/main/java/org/tango/web/server/Responses.java
@@ -2,23 +2,28 @@ package org.tango.web.server;
 
 import com.google.common.base.Function;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.gson.Gson;
 import com.sun.javafx.binding.StringFormatter;
 import com.sun.org.apache.xpath.internal.operations.*;
 import fr.esrf.Tango.DevError;
 import fr.esrf.Tango.DevFailed;
+import fr.esrf.Tango.ErrSeverity;
 import org.tango.web.rest.Response;
 
 import java.io.Writer;
 import java.lang.String;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author ingvord
  * @since 5/24/14@8:10 PM
  */
 public class Responses {
+
+
     private final static Gson GSON = new Gson();
 
     private Responses() {
@@ -63,34 +68,33 @@ public class Responses {
     }
 
     public static <T> Response<T> createFailureResult(Throwable cause) {
-        return new Response<>(null, createExceptionMessage(cause), "INVALID", System.currentTimeMillis());
+        return new Response<>(null, throwableToError(cause), Response.FAILURE, System.currentTimeMillis());
     }
 
     public static <T> Response<T> createFailureResult(String message) {
-        return new Response<>(null, new String[]{message}, "INVALID", System.currentTimeMillis());
+        return new Response<>(null, throwableToError(new Exception(message, null)), Response.FAILURE, System.currentTimeMillis());
     }
 
     public static <T> Response<T> createFailureResult(String message, Throwable cause) {
-        return new Response<>(null, createExceptionMessage(new Exception(message, cause)), "INVALID", System.currentTimeMillis());
+        return new Response<>(null, throwableToError(new Exception(message, cause)), Response.FAILURE, System.currentTimeMillis());
     }
 
     public static <T> Response<T> createFailureResult(DevFailed devFailed) {
-        return new Response<>(null, Iterables.toArray(Iterables.transform(
-                Arrays.asList(devFailed.errors), new Function<DevError, String>() {
+        return new Response<>(null, Lists.transform(
+                Arrays.asList(devFailed.errors), new Function<DevError, Response.Error>() {
                     @Override
-                    public String apply(DevError input) {
-                        return String.format("%s: %s - %s", input.severity, input.reason, input.desc);
+                    public Response.Error apply(DevError input) {
+                        return new Response.Error(input.reason,input.desc,input.severity.toString(),input.origin);
                     }
-                }), String.class), "INVALID", System.currentTimeMillis());
+                }).toArray(new Response.Error[devFailed.errors.length]), Response.FAILURE, System.currentTimeMillis());
     }
 
-    public static <T> Response<T> createFailureResult(String msg, DevFailed devFailed) {
-        return new Response<>(null, Iterables.toArray(Iterables.concat(
-                Arrays.asList(msg), Iterables.transform(Arrays.asList(devFailed.errors), new Function<DevError, String>() {
-                    @Override
-                    public String apply(DevError input) {
-                        return String.format("%s: %s - %s", input.severity, input.reason, input.desc);
-                    }
-                })), String.class), "INVALID", System.currentTimeMillis());
+    public static Response.Error[] throwableToError(Throwable throwable){
+        List<Response.Error> result = new ArrayList<>();
+        do{
+            result.add(new Response.Error(
+                    throwable.getClass().getSimpleName(), throwable.getMessage(),ErrSeverity.ERR.toString(),throwable.getStackTrace()[0].toString()));
+        } while ((throwable = throwable.getCause()) != null);
+        return result.toArray(new Response.Error[result.size()]);
     }
 }
