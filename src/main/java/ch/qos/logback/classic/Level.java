7c18260323aa70b0ec82b305c7158870910b25db
Igor Khokhriakov <igor.khokhriakov@hzg.de>
2015-12-15 13:03:07 +0100

    web-frontend: minor changes

--- src/main/java/ch/qos/logback/classic/Level.java
+++ src/main/java/ch/qos/logback/classic/Level.java
@@ -1,6 +1,8 @@
 package ch.qos.logback.classic;
 
 /**
+ * Work around JTangoServer compilation failure when no logback classes are provided
+ *
  * @author Igor Khokhriakov <igor.khokhriakov@hzg.de>
  * @since 14.12.2015
  */
