2e27d69548b863fae749d583c30cda7e724f2e03
Ingvord <Ingvord@users.noreply.github.com>
2017-02-17 13:43:14 +0100

    Progress #124

---
+++ src/test-integration/java/org.tango.web.server/command/ITCommandImplTest.java
@@ -0,0 +1,41 @@
+package org.tango.web.server.command;
+
+import org.junit.Test;
+import org.tango.client.ez.proxy.TangoProxies;
+import org.tango.client.ez.proxy.TangoProxy;
+
+import java.lang.reflect.Method;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * @author Igor Khokhriakov <igor.khokhriakov@hzg.de>
+ * @since 15.10.12
+ */
+//TODO make these tests environment independent
+public class ITCommandImplTest {
+    private final String tangoHost = System.getenv("TANGO_HOST");
+
+    @Test
+    public void testExecute_WriteRead() throws Exception {
+        TangoProxy proxy = TangoProxies.newDeviceProxyWrapper("tango://" + tangoHost + "/sys/tg_test/1");
+        Method mtd = proxy.getClass().getMethod("writeAttribute", String.class, Object.class);
+        Command cmd = new Command(proxy, mtd, "double_scalar_w", Math.PI);
+
+        cmd.execute();
+
+        mtd = proxy.getClass().getMethod("readAttribute", String.class);
+        cmd = new Command(proxy, mtd, "double_scalar_w");
+
+        assertEquals(Math.PI, cmd.execute());
+    }
+
+    @Test
+    public void testExecute_Exec() throws Exception {
+        TangoProxy proxy = TangoProxies.newDeviceProxyWrapper("tango://" + tangoHost + "/sys/tg_test/1");
+        Method mtd = proxy.getClass().getMethod("executeCommand", String.class, Object.class);
+        Command cmd = new Command(proxy, mtd, "DevDouble", Math.E);
+
+        assertEquals(Math.E, cmd.execute());
+    }
+}
