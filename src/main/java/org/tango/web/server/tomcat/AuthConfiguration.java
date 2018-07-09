f37f1d1051bd1001e3b3a4f8cd1672001a127bb5
ingvord <tla-ingvord@yandex.ru>
2018-07-09 15:49:44 +0300

    Integrate with LDAP

--- src/main/java/org/tango/web/server/tomcat/AuthConfiguration.java
+++ src/main/java/org/tango/web/server/tomcat/AuthConfiguration.java
@@ -1,6 +1,8 @@
 package org.tango.web.server.tomcat;
 
+import de.hzg.wpi.utils.authorization.AuthorizationMechanism;
 import de.hzg.wpi.utils.authorization.Kerberos;
+import de.hzg.wpi.utils.authorization.Ldap;
 import de.hzg.wpi.utils.authorization.PlainText;
 import org.apache.catalina.startup.Tomcat;
 import org.jboss.resteasy.specimpl.MultivaluedMapImpl;
@@ -9,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.tango.TangoRestServer;
 
 import javax.ws.rs.core.MultivaluedMap;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,33 +21,45 @@ import java.util.Map;
  * @since 04.02.2016
  */
 public class AuthConfiguration {
+    public static final String LDAP_PROPERTIES = "LDAP_PROPERTIES";
     private final Logger logger = LoggerFactory.getLogger(AuthConfiguration.class);
 
     public static final String[] DEFAULT_ROLES = {"desy-user", "mtango-rest", "mtango-groovy"};
 
     private Map<String, String> users = new HashMap<>();
     private MultivaluedMap<String, String> roles = new MultivaluedMapImpl<>();
-    private String authMethod;
+    private AuthorizationMechanism authMethod;
 
     public AuthConfiguration(String authMethod, String[] users, String[] passwords) {
-        this.authMethod = authMethod;
         for (int i = 0; i < users.length; ++i) {
             this.users.put(users[i], passwords[i]);
             this.roles.put(users[i], Arrays.asList(DEFAULT_ROLES));
         }
+        this.authMethod = createAuthorizationMechanism(authMethod);
+    }
+
+    private AuthorizationMechanism createAuthorizationMechanism(String authMethod){
+        logger.debug("Configure tomcat auth {} for device", authMethod);
+        try {
+            switch (authMethod) {
+                case "plain":
+                    return new PlainText(users, roles);
+                case "kerberos":
+                    return new Kerberos(TangoRestServer.class.getSimpleName());
+                case "ldap":
+                    return new Ldap(System.getProperty(LDAP_PROPERTIES, "ldap.properties"));
+                default:
+                    throw new IllegalArgumentException("Unknown auth method - " + authMethod);
+            }
+        } catch (IOException e) {
+            logger.error("Failed to create LDAP auth configuration!", e);
+            throw new RuntimeException(e);
+        }
     }
 
     public void configure(Tomcat tomcat){
-        logger.debug("Configure tomcat auth for device");
-        switch (authMethod) {
-            case "plain":
-                PlainText plainText = new PlainText(tomcat, users, roles);
-                plainText.configure();
-                break;
-            case "kerberos":
-                Kerberos kerberos = new Kerberos(tomcat, TangoRestServer.class.getSimpleName());
-                kerberos.configure();
-                break;
-        }
+        logger.debug("Configuring tomcat auth for device...");
+        authMethod.configure(tomcat);
+        logger.debug("Done!");
     }
 }
