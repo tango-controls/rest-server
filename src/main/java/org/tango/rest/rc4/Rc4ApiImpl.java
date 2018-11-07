6f9de4377629ad3bfcbd247e3f4ea1dcd2422cb5
ingvord <mail@ingvord.ru>
2018-11-07 13:23:12 +0100

    Progress: #164

--- src/main/java/org/tango/rest/rc4/Rc4ApiImpl.java
+++ src/main/java/org/tango/rest/rc4/Rc4ApiImpl.java
@@ -2,6 +2,7 @@ package org.tango.rest.rc4;
 
 import com.google.common.base.Function;
 import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,23 +10,19 @@ import org.tango.TangoRestServer;
 import org.tango.client.ez.proxy.TangoProxyException;
 import org.tango.rest.Device;
 import org.tango.rest.Devices;
+import org.tango.rest.DevicesTree;
 import org.tango.rest.SupportedAuthentication;
+import org.tango.rest.entities.DeviceFilters;
 import org.tango.web.server.DatabaseDs;
 import org.tango.web.server.binding.Partitionable;
 import org.tango.web.server.binding.StaticValue;
 
 import javax.annotation.Nullable;
 import javax.servlet.ServletContext;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
 import java.net.URI;
-import java.util.AbstractMap;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author Igor Khokhriakov <igor.khokhriakov@hzg.de>
@@ -34,6 +31,7 @@ import java.util.Map;
 @Path("/rc4")
 @Produces("application/json")
 public class Rc4ApiImpl {
+    public static final String DEFAULT_TANGO_PORT = "10000";
     private final Logger logger = LoggerFactory.getLogger(Rc4ApiImpl.class);
 
     @GET
@@ -91,5 +89,26 @@ public class Rc4ApiImpl {
     @Path("/hosts/{host}/{port}/devices/{domain}/{family}/{member}")
     public Device getDevice() {
         return new Device();
+    }
+
+    //    @Path("/hosts/{var:tango_host}/devices/tree")
+//    public DevicesTree getDevicesTree(@PathParam("var") PathSegment tango_host,
+//                                      @QueryParam("f") List<String> filters) {
+    @Path("/hosts/{host}/{port}/devices/tree")
+    public DevicesTree getDevicesTree(@PathParam("host") String host,
+                                      @PathParam("port") String port,
+                                      @QueryParam("f") List<String> filters) {
+        return getDevicesTree(Lists.newArrayList(host + ":" + port), filters);
+    }
+
+    @Path("/hosts/tree")
+    public DevicesTree getDevicesTree(@QueryParam("v") List<String> tango_hosts,
+                                      @QueryParam("f") List<String> filters) {
+        DeviceFilters filter = new DeviceFilters(filters.toArray(new String[filters.size()]));
+
+        Iterable<String> it =
+                new LinkedList<>(tango_hosts);
+
+        return new DevicesTree(it, filter);
     }
 }
