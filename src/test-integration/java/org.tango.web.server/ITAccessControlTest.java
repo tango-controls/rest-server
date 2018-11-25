433817ac681f6d38f83596b80884d0c4a5f655a2
ingvord <tla-ingvord@yandex.ru>
2018-11-25 16:40:02 +0300

    Progress: #168

--- src/test-integration/java/org.tango.web.server/ITAccessControlTest.java
+++ src/test-integration/java/org.tango.web.server/ITAccessControlTest.java
@@ -1,20 +1,16 @@
 package org.tango.web.server;
 
 
-import org.junit.Before;
 import org.junit.Test;
 import org.tango.client.ez.proxy.TangoProxies;
 import org.tango.client.ez.proxy.TangoProxy;
-import org.tango.rest.test.Rc4Test;
 
 import static org.junit.Assert.assertTrue;
 
 public class ITAccessControlTest {
-    private final String tangoHost = System.getenv("TANGO_HOST");
-
     @Test
     public void testCheckUserCanRead() throws Exception {
-        TangoProxy proxy = TangoProxies.newDeviceProxyWrapper("tango://" + tangoHost + "/sys/access_control/1");
+        TangoProxy proxy = TangoProxies.newDeviceProxyWrapper("tango://localhost:10000/sys/access_control/1");
         AccessControl instance = new AccessControl(proxy);
 
         assertTrue(instance.checkUserCanRead("test", "127.0.0.1", "sys/tg_test/1"));
